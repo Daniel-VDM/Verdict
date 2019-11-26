d7a3d52049e331394da3d11ea5322a1f83e1af4a
Aarti Lalwani <aartilalwani@Aartis-Air.attlocal.net>
2019-11-26 15:10:53 -0800

    added alan's code

---
+++ app/src/main/java/io/verdict/ui/forum/PostResponseActivity.java
@@ -0,0 +1,79 @@
+package io.verdict;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class PostResponseActivity extends AppCompatActivity {
+
+    Button submitButton;
+    EditText threadThumbs;
+    EditText threadTitle;
+    EditText response;
+    CheckBox isAnonymous;
+    int thumbs;
+    String title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_response);
+
+        threadThumbs = (EditText) findViewById(R.id.postResponse_titleNumThumbs);
+        threadTitle = (EditText) findViewById(R.id.postResponse_titleName);
+        response = (EditText) findViewById(R.id.postResponse_responseContent);
+        isAnonymous = (CheckBox) findViewById(R.id.postResponse_anonymous);
+        submitButton = (Button) findViewById(R.id.postResponse_submitButton);
+
+        Intent i = getIntent();
+        thumbs = i.getExtras().getInt("threadThumbs", 0);
+        title = i.getExtras().getString("threadTitle", "Error");
+
+        if (title != "Error") {
+            threadThumbs.setText(String.valueOf(thumbs));
+            threadTitle.setText(title);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PostResponseActivity.this, SingleThreadActivity.class);
+                intent.putExtra("threadThumbs", thumbs);
+                intent.putExtra("threadTitle", title);
+                intent.putExtra("response", response.getText().toString());
+                intent.putExtra("anonymous", isAnonymous.isChecked());
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() { super.onDestroy();
+    }
+
+}
